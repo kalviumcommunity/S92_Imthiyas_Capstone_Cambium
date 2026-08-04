5dc2ad3463b048808a0cf20ed39e6c14661e617a
shaikimthiyas <shaik.imthiyas.t@gmail.com>
2026-08-04 23:46:20 +0530

    Create React frontend components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Cambium</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Research</li>
+        <li>Opportunities</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
