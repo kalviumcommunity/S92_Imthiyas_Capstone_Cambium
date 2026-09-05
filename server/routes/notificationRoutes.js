d2b4493fd69a4d99c031a336dc9781fcf5543421
shaikimthiyas <shaik.imthiyas.t@gmail.com>
2026-09-05 21:54:08 +0530

    Implement POST API endpoints for Cambium research platform

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const {
   getNotifications,
   getUserNotifications,
+  createNotification,
 } = require("../controllers/notificationController");
 
-router.route("/").get(getNotifications);
+router.route("/").get(getNotifications).post(createNotification);
 router.route("/user/:userId").get(getUserNotifications);
 
 module.exports = router;
